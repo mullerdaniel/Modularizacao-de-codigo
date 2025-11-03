d0af0acbbbb03b4fd47fbc1ba546eb670e3d1f1d
Daniel Müller <miladaniel370@gmail.com>
2025-11-03 20:40:47 -0300

    DM-11 feat: Metodo para cadastrar emprestimo

--- src/main/java/org/example/Model/Emprestimos.java
+++ src/main/java/org/example/Model/Emprestimos.java
@@ -1,5 +1,6 @@
 package org.example.Model;
 
+import java.sql.Date;
 import java.time.LocalDateTime;
 
 public class Emprestimos {
--- src/main/java/org/example/Service/LivroService.java
+++ src/main/java/org/example/Service/LivroService.java
@@ -55,5 +55,4 @@ public class LivroService {
 
         return livros;
     }
-
 }
