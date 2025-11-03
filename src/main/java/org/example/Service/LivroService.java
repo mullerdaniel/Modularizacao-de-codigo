fc9869b96f94fdeb91eaaca995ae8c7ccd8eb27f
Daniel Müller <miladaniel370@gmail.com>
2025-11-03 20:32:32 -0300

    DM-10 feat: Metodo para listar todos os livros

--- src/main/java/org/example/Service/LivroService.java
+++ src/main/java/org/example/Service/LivroService.java
@@ -5,7 +5,10 @@ import org.example.Model.Livros;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class LivroService {
 
@@ -23,6 +26,34 @@ public class LivroService {
             stmt.setBoolean(4, livros.isDisponivel());
             stmt.executeUpdate();
         }
+    }
+
+
+    // METODO PARA LISTAR TODOS OS LIVROS CADASTRADOS
+    public List<Livros> listarLivros() throws SQLException {
+        List<Livros> livros = new ArrayList<>();
+        String query = "SELECT id, titulo, autor, ano, disponivel FROM livros";
+
+        try(Connection conn = Conexao.conectar();
+            PreparedStatement stmt = conn.prepareStatement(query)) {
+
+            ResultSet rs = stmt.executeQuery();
+            while(rs.next()) {
+                Livros livro = new Livros(
+                        rs.getInt("id"),
+                        rs.getString("titulo"),
+                        rs.getString("autor"),
+                        rs.getInt("ano"),
+                        rs.getBoolean("disponivel")
+                );
+                livros.add(livro);
+            }
+
+        }catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return livros;
     }
 
 }
