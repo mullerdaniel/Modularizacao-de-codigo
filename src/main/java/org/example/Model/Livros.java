2ef9b57bc54d23a795944f7ee2ff6aee60dd067a
Daniel Müller <miladaniel370@gmail.com>
2025-11-03 19:42:52 -0300

    DM-4 feat: Incrementando entidades na classe livros

---
+++ src/main/java/org/example/Model/Livros.java
@@ -0,0 +1,68 @@
+package org.example.Model;
+
+public class Livros {
+
+    private int id;
+    private String titulo;
+    private String autor;
+    private int ano;
+    private boolean disponivel; // POR PADRÃO É TRUE
+
+
+    // METODO CONSTRUTOR COM E SEM ID
+    public Livros(int id, String titulo, String autor, int ano, boolean disponivel) {
+        this.id = id;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.ano = ano;
+        this.disponivel = disponivel;
+    }
+
+    public Livros(String titulo, String autor, int ano, boolean disponivel) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.ano = ano;
+        this.disponivel = disponivel;
+    }
+
+    // GETs e SETs
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    public boolean isDisponivel() {
+        return disponivel;
+    }
+
+    public void setDisponivel(boolean disponivel) {
+        this.disponivel = disponivel;
+    }
+}
