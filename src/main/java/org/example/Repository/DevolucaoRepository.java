d3a160f4f733070aed3077cff50ff4e97033489d
Daniel Müller <miladaniel370@gmail.com>
2025-11-06 20:03:59 -0300

    DM-18 feat: Metodo para devolução de livros

--- src/main/java/org/example/Repository/DevolucaoRepository.java
+++ src/main/java/org/example/Repository/DevolucaoRepository.java
@@ -7,11 +7,11 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
-public class EmprestimoRepository {
+public class DevolucaoRepository {
 
 
-    // METODO PARA REGISTRAR EMPRESTIMO DE LIVRO
-    public void registrarEmprestimoDeLivro(Emprestimos emprestimos) throws SQLException {
+    // METODO PARA REGISTRAR DEVOLUÇÃO DO LIVRO
+    public void registrarDevolucaoDeLivro(Emprestimos emprestimos) throws SQLException {
         String query = "INSERT INTO emprestimos (livro_id, usuario_id, data_emprestimo, data_devolucao) VALUES (?,?,?,?)";
 
         try(Connection conn = Conexao.conectar();
@@ -19,16 +19,9 @@ public class EmprestimoRepository {
 
             stmt.setInt(1, emprestimos.getLivro_id());
             stmt.setInt(2, emprestimos.getUsuario_id());
-            stmt.setTimestamp(3, java.sql.Timestamp.valueOf(emprestimos.getData_emprestimo()));
-            java.time.LocalDateTime dataDevolucao = emprestimos.getData_devolucao();
-
-            if (dataDevolucao != null) {
-                stmt.setTimestamp(4, java.sql.Timestamp.valueOf(dataDevolucao));
-            } else {
-                stmt.setTimestamp(4, null);
-            }
+            stmt.setTimestamp(3,java.sql.Timestamp.valueOf(emprestimos.getData_emprestimo()));
+            stmt.setTimestamp(4, java.sql.Timestamp.valueOf(emprestimos.getData_devolucao()));
             stmt.executeUpdate();
         }
     }
-
 }
